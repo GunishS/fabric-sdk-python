52291d145da7ce69c573ae299b8dd7dbc0484f8a
GunishS <gunish99@gmail.com>
2025-06-21 00:45:46 +0530

    funtion to run graphQL added & pagination for workspace search

--- core/client.py
+++ core/client.py
@@ -23,3 +23,35 @@ class FabricClient:
         )
         response.raise_for_status()
         return response.json()
+    
+    def list_workspaces(self):
+        url = f"{self.base_url}/workspaces"
+        response = requests.get(
+            url, headers=self.get_headers()
+        )
+        response.raise_for_status()
+        return response.json()
+    
+    def list_all_workspaces(self):
+        url = f"{self.base_url}/workspaces"
+        all_workspaces = []
+        while url:
+            response = requests.get(url, headers=self.get_headers())
+            response.raise_for_status()
+            data = response.json()
+            all_workspaces.extend(data.get('value', []))
+            url = data.get('nextLink')  # If API supports pagination
+        return all_workspaces
+    
+    def run_graphql_query(self, graphql_endpoint, query):
+        token = self.authenticator.get_token("https://analysis.windows.net/powerbi/api/.default")
+        headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+        response = requests.post(
+            graphql_endpoint,
+            json={'query': query},
+            headers=headers
+        )
+        response.raise_for_status()
+        return response.json()
+
+
